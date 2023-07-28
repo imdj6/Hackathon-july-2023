8834a7faf5756e7ef6d28e25f2fa14fcda2a8b22
Danish Jamal <mdddanishjamal04@yahoo.com>
2023-07-28 13:45:16 +0530

    added new files

--- Backend/db.js
+++ Backend/db.js
@@ -1,10 +1,11 @@
-const mongoose =require('mongoose');
-const mongoURI='mongodb+srv://mddanishjamal04:cKOrIVhzxSdAVR7U@chatboat.10hmkoj.mongodb.net/?retryWrites=true&w=majority';
+const mongoose = require("mongoose");
+const mongoURI =
+  "mongodb+srv://mddanishjamal04:cKOrIVhzxSdAVR7U@chatboat.10hmkoj.mongodb.net/?retryWrites=true&w=majority";
 
-const connectToMongo=()=>{
-    mongoose.connect(mongoURI).then(()=>{
-        console.log("connected");
-    })
-}
+const connectToMongo = () => {
+  mongoose.connect(mongoURI).then(() => {
+    console.log("connected");
+  });
+};
 
-module.exports=connectToMongo;
+module.exports = connectToMongo;
