9dc0d830a059bbfd7f721fd452f25cac7329fa5d
MD DANISH JAMAL <81770608+imdj6@users.noreply.github.com>
2023-07-29 23:24:45 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const mongoURI =
-  "mongodb+srv://mddanishjamal04:cKOrIVhzxSdAVR7U@chatboat.10hmkoj.mongodb.net/?retryWrites=true&w=majority";
+  "";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI).then(() => {
